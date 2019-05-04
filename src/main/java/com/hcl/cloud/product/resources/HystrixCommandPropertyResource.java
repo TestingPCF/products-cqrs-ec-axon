d974f3e445c38feb7990d246fc5d5ada99488cfa
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-05-04 18:46:03 +0530

    code changes according to sonar suggestion.

--- src/main/java/com/hcl/cloud/product/resources/HystrixCommandPropertyResource.java
+++ src/main/java/com/hcl/cloud/product/resources/HystrixCommandPropertyResource.java
@@ -1,13 +1,16 @@
 package com.hcl.cloud.product.resources;
 
-import java.util.Set;
-
-import org.springframework.stereotype.Component;
-
+import static com.hcl.cloud.product.constants.ProductConstants.COLLAPSER;
+import static com.hcl.cloud.product.constants.ProductConstants.COMMAND_KEY_IDENTIFIER;
 import static com.hcl.cloud.product.constants.ProductConstants.PROP_VAL_PATTERN;
 import static com.hcl.cloud.product.constants.ProductConstants.THREAD_POOL_IDENTIFIER;
-import static com.hcl.cloud.product.constants.ProductConstants.COMMAND_KEY_IDENTIFIER;
-import static com.hcl.cloud.product.constants.ProductConstants.COLLAPSER;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 import com.netflix.config.ConfigurationManager;
 
 /**
@@ -18,6 +21,7 @@ import com.netflix.config.ConfigurationManager;
 @Component
 public class HystrixCommandPropertyResource {
 
+    static Logger log = LoggerFactory.getLogger(HystrixCommandPropertyResource.class);
     public HystrixCommandPropertyResource() {
 
         PropertyResourceManager hystrixProperties = new PropertyResourceManager("/HystrixCommand.properties");
@@ -26,29 +30,8 @@ public class HystrixCommandPropertyResource {
             String key = (String) k;
             String value = hystrixProperties.getPropertyValue(key);
             if (value != null) {
-                if (key.contains(COMMAND_KEY_IDENTIFIER)) {
-                    if (value.matches(PROP_VAL_PATTERN)) {
-                        ConfigurationManager.getConfigInstance().setProperty(key, Long.valueOf(value));
-
-                    } else if ("true".equals(value) || "false".equals(value)) {
-                        ConfigurationManager.getConfigInstance().setProperty(key, Boolean.valueOf(value));
-
-                    } else {
-                        ConfigurationManager.getConfigInstance().setProperty(key, value);
-
-                    }
-                } else if (key.contains(THREAD_POOL_IDENTIFIER)) {
-                    if (value.matches(PROP_VAL_PATTERN)) {
-                        ConfigurationManager.getConfigInstance().setProperty(key, Long.valueOf(value));
-
-                    } else if ("true".equals(value) || "false".equals(value)) {
-                        ConfigurationManager.getConfigInstance().setProperty(key, Boolean.valueOf(value));
-
-                    } else {
-                        ConfigurationManager.getConfigInstance().setProperty(key, value);
-
-                    }
-                } else if (key.contains(COLLAPSER)) {
+                if (key.contains(COMMAND_KEY_IDENTIFIER) || key.contains(THREAD_POOL_IDENTIFIER)
+                        || key.contains(COLLAPSER)) {
                     if (value.matches(PROP_VAL_PATTERN)) {
                         ConfigurationManager.getConfigInstance().setProperty(key, Long.valueOf(value));
 
@@ -60,6 +43,9 @@ public class HystrixCommandPropertyResource {
 
                     }
                 }
+
+            }else {
+                log.info("HystrixCommand.properties having key with null value. The key is :" +value);
             }
         }
     }
